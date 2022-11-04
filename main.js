f03bfe8bcaf9678942d44bb6e5a1793400aac85e
Yordy <yoalmonte.dev@outlook.com>
2022-11-03 23:19:56 -0400

    Ligero avance en el proyecto

---
+++ main.js
@@ -0,0 +1,8 @@
+const email = document.getElementById('email');
+const money = document.getElementById('money');
+const tel = document.getElementById('tel');
+const date = document.getElementById('date');
+const creditCard = document.getElementById('credit-card');
+const primaryBtn = document.getElementById('primary-button');
+const secondaryBtn = document.getElementById('secondary-button');
+
