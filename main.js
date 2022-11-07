87efb7dda6bd6b0cef8f0a254083809e51ad19ca
Yordy <yoalmonte.dev@outlook.com>
2022-11-06 21:49:54 -0400

    Terminado SecondaryBtn

--- main.js
+++ main.js
@@ -3,6 +3,51 @@ const money = document.getElementById('money');
 const tel = document.getElementById('tel');
 const date = document.getElementById('date');
 const creditCard = document.getElementById('credit-card');
-const primaryBtn = document.getElementById('primary-button');
-const secondaryBtn = document.getElementById('secondary-button');
 
+
+
+
+const emailValidation = () => {
+
+}
+
+
+
+const moneyFormat = () => {
+    money.value
+}
+
+
+
+const telValidation = () => {
+
+}
+
+
+
+const dateValidation = () => {
+
+}
+
+
+
+const creditCardValidation = () => {
+
+}
+
+
+
+const primaryBtn = () => {
+
+    document.getElementById('primary-button');   
+}
+
+
+
+const secondaryBtn = () => {
+    email.value = '';
+    money.value = '';
+    tel.value = '';
+    date.value = '';
+    creditCard.value = '';
+}
